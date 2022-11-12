bdf7f2a9426ee0d29e6631f49379db8ae978e571
Joselito Guardado <jaguardado123@gmail.com>
2022-11-12 15:31:38 -0600

    Created basic map using ol package

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('Hello Workshop');
+// Bring in required modules from ol package.
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {Map, View} from 'ol';
+import {fromLonLat} from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
+// Create our map.
+const map = new Map({
+    target: 'map-container', // Points to our div container.
+    layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
+    ],
+    view: new View({
+        center: fromLonLat([0,0]),
+        zoom: 2
+    }),
+});
