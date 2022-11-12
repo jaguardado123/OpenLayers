a173582e212a62986ba80579adc3f457a1dc744e
Joselito Guardado <jaguardado123@gmail.com>
2022-11-12 15:38:11 -0600

    Loading map from GeoJSON file

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 // Bring in required modules from ol package.
+import GeoJSON from 'ol/format/GeoJSON';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import {Map, View} from 'ol';
 import {fromLonLat} from 'ol/proj';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import Link from 'ol/interaction/link';
 
 // Create our map.
 const map = new Map({
     target: 'map-container', // Points to our div container.
     layers: [
-        new TileLayer({
-            source: new OSM(),
+        // Load map from OpenStreetMaps.
+        // new TileLayer({
+        //     source: new OSM(),
+        // }),
+        // Load map from GeoJSON file.
+        new VectorLayer({
+            source: new VectorSource({
+                format: new GeoJSON(),
+                url: './data/countries.json',
+            }),
         }),
     ],
     view: new View({
@@ -19,3 +29,6 @@ const map = new Map({
         zoom: 2
     }),
 });
+
+// Remember current positon after reloading page.
+map.addInteraction(new Link());
